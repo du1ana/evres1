dc485b73644a05a919c878d3bf09830f3434cbba
Dulana Peiris <dulthar@gmail.com>
2023-11-15 13:42:57 +0530

    removed junk files

--- etag_test/etag-test.sh
+++
@@ -1,8 +0,0 @@
-branch="deploy"
-url="https://api.github.com/repos/du1ana/ev-res-test/commits?sha=$branch"
-
-headers=$(curl -s -I -X GET "$url")
-
-etag_value=$(echo "$headers" | grep -i etag: | awk -F ': ' '{print $2}')
-
-echo "The etag value is: $etag_value"
