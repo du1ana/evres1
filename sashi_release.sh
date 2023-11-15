6a1382b111e4720bd6c98e06369716fb56b392a4
Dulana Peiris <dulthar@gmail.com>
2023-11-15 13:30:18 +0530

    tag prefix bug fix

--- sashi_release.sh
+++ sashi_release.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
-tag_version="$1"
+tag_version="sashi_$1"
 release_note="$2"
 
-gh release create "$tag_version" ./installer/* -t "sashi_$tag_version" -n "$release_note"
+gh release create "$tag_version" ./installer/* -t "$tag_version" -n "$release_note"
