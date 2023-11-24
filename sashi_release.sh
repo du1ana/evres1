29ed92609f8a7348a74bfa9d0f9c87831b66d251
Dulana Peiris <dulthar@gmail.com>
2023-11-24 14:12:48 +0530

    release script update

--- sashi_release.sh
+++ sashi_release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Invalid parameters. Usage: $0 <tag_version> <release_note>"
+if [ "$#" -ne 3 ]; then
+    echo "Invalid parameters. Usage: $0 <tag_version> <target_branch> <release_note>"
     exit 1
 fi
 
 tag_version="sashi_$1"
-release_note="$2"
+target_branch="$2"
+release_note="$3"
 
-gh release create "$tag_version" ./installer/* -t "$tag_version" -n "$release_note"
+gh release create "$tag_version" ./installer/* -t "$tag_version" --target "$target_branch" -n "$release_note"
