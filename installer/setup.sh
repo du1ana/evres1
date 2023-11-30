829b43cdcb3a09be88f52a553edb29467bead392
Dulana Peiris <dulthar@gmail.com>
2023-11-30 15:16:24 +0530

    added to sudo list

--- installer/setup.sh
+++ installer/setup.sh
@@ -207,11 +207,12 @@ else
 fi
 mode=$1
 
-if [ "$mode" == "install" ] || [ "$mode" == "uninstall" ] || [ "$mode" == "update" ] || [ "$mode" == "log" ] || [ "$mode" == "transfer" ]  || [ "$mode" == "set-regkey" ] ; then
+if [ "$mode" == "install" ] || [ "$mode" == "uninstall" ] || [ "$mode" == "update" ] || [ "$mode" == "log" ] || [ "$mode" == "transfer" ] ; then
     [ -n "$2" ] && [ "$2" != "-q" ] && [ "$2" != "-i" ] && echo "Second arg must be -q (Quiet) or -i (Interactive)" && exit 1
     [ "$2" == "-q" ] && interactive=false || interactive=true
     [ "$mode" == "transfer" ] && transfer=true || transfer=false
-    (! $transfer || $installed) && [ "$EUID" -ne 0 ] && echo "Please run with root privileges (sudo)." && exit 1
+    [ "$mode" == "set-regkey" ] && set_regkey=true || set_regkey=false
+    (! $transfer || $installed || $set_regkey) && [ "$EUID" -ne 0 ] && echo "Please run with root privileges (sudo)." && exit 1
 fi
 
 # Change the relevant setup helper path based on Evernode installation condition and the command mode.
